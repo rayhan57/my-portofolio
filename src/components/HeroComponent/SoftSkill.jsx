d9b8db55863204571b3f31067439bf02aa848b3f
rayhan57 <rayhanb18@gmail.com>
2024-02-07 18:38:58 +0700

    add container

--- src/components/HeroComponent/SoftSkill.jsx
+++ src/components/HeroComponent/SoftSkill.jsx
@@ -6,34 +6,36 @@ import timeManagement from "../../assets/img/softSkill/time-management.png";
 
 const SoftSkill = () => {
   return (
-    <div className="absolute bottom-0 left-0 flex w-full items-center justify-evenly bg-primary py-2 text-sm font-medium lg:py-5 lg:text-lg">
-      <div>
-        <img
-          src={teamWork}
-          className="mx-auto w-9 lg:w-12"
-          alt="time-management"
-        />
-        <h2>Team Work</h2>
-      </div>
-      <div>
-        <img
-          src={problemSolving}
-          className="mx-auto w-9 lg:w-12"
-          alt="problem-solving"
-        />
-        <h2>Problem Solving</h2>
-      </div>
-      <div>
-        <img src={accuracy} className="mx-auto w-9 lg:w-12" alt="accuracy" />
-        <h2>Accuracy</h2>
-      </div>
-      <div>
-        <img
-          src={timeManagement}
-          className="mx-auto w-9 lg:w-12"
-          alt="time-management"
-        />
-        <h2>Time Management</h2>
+    <div className="absolute bottom-0 left-0 w-full bg-primary py-2 text-sm font-medium lg:py-5 lg:text-lg">
+      <div className="container flex items-center justify-around">
+        <div>
+          <img
+            src={teamWork}
+            className="mx-auto w-9 lg:w-12"
+            alt="time-management"
+          />
+          <h2>Team Work</h2>
+        </div>
+        <div>
+          <img
+            src={problemSolving}
+            className="mx-auto w-9 lg:w-12"
+            alt="problem-solving"
+          />
+          <h2>Problem Solving</h2>
+        </div>
+        <div>
+          <img src={accuracy} className="mx-auto w-9 lg:w-12" alt="accuracy" />
+          <h2>Accuracy</h2>
+        </div>
+        <div>
+          <img
+            src={timeManagement}
+            className="mx-auto w-9 lg:w-12"
+            alt="time-management"
+          />
+          <h2>Time Management</h2>
+        </div>
       </div>
     </div>
   );
